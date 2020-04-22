9072f5a6a5cbd55ac88ec801499abbde02c7804d
laijia <laijia2008@126.com>
2020-04-22 16:04:13 +0800

    add gen cpp

---
+++ gen_cpp_by_xml.py
@@ -0,0 +1,82 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import codecs
+import xml.etree.ElementTree as ET
+
+############################################################################
+rpc_id = 'RPC_%(module)s_%(msg_name)s_REQ'
+handle_field = 'g_pPacketMgr->RegisterHandle(%(msg_id)s, Handle%(module)s::%(msg_name)sReq);'
+packet_field = 'g_pPacketMgr->RegisterPacket(%(msg_id)s, new CPacket<%(module)s_%(msg_name)sReq>());'
+func_field = 'static int %(msg_name)sReq(Player* player, Packet* packet);'
+############################################################################
+
+class GenCpp(object):
+    def __init__(self):
+        self.root = None
+        self.module = ""
+        self.enum_fields = ""
+        self.handle_fields = ""
+        self.func_fields = ""
+        self.module_id = 0
+        pass
+    
+    def load_xml(self, xml_file):
+        contents = ""
+        with codecs.open(xml_file, "r", "utf-8") as f:
+            contents = f.read()
+        if contents == "":
+            return
+        self.proto_file = os.path.splitext(xml_file)[0] + ".proto"
+        print(self.proto_file)
+        self.root = ET.fromstring(contents)
+        self.module = self.root.attrib['name']
+        print(self.root)
+        pass
+
+    def write_cpp(self):
+        s = ""
+        with codecs.open("./proto_cpp.tmpl", "r", "utf-8") as f:
+            s = f.read()
+        s = s % {
+                    "module":self.module,
+                    "module_upper":self.module.upper(),
+                    "module_id":self.module_id,
+                    "enum_fields":self.enum_fields, 
+                    "handle_fields":self.handle_fields,
+                    "func_fields":self.func_fields
+                }
+
+        with codecs.open("Handle"+self.module+".h", "w", "utf-8") as f:
+            f.write(s)
+            f.flush()
+        pass
+
+    def parse_xml(self):
+        self.module_id = self.root[0].text
+        for req_reply in self.root.findall("Message/ReqReplyMsg"):
+            msg_id = req_reply.attrib["id"]
+            msg_name = req_reply.attrib["name"]
+            id_field = rpc_id % {"module":self.module, "msg_name":msg_name}
+            id_field = id_field.upper()
+            enum_field = id_field +" = "+msg_id+",\n\t\t"
+            self.enum_fields += enum_field
+            self.handle_fields += handle_field % {"msg_id":id_field, "module":self.module, "msg_name":msg_name}
+            self.handle_fields +='\n\t\t'
+            self.handle_fields += packet_field % {"msg_id":id_field, "module":self.module, "msg_name":msg_name}
+            self.handle_fields +='\n\t\t'
+            self.func_fields += func_field % {"msg_name":msg_name}
+            self.func_fields +='\n\t'
+        
+        self.enum_fields = self.enum_fields[:-3]
+        self.handle_fields = self.handle_fields[:-3]
+        self.func_fields = self.func_fields[:-2]
+        pass
+
+if __name__ == "__main__":
+    gen_cpp = GenCpp()
+    gen_cpp.load_xml("ModuleChat.xml")
+    gen_cpp.parse_xml()
+    gen_cpp.write_cpp()
+    pass
--- gen_proto_by_xml.py
+++ gen_proto_by_xml.py
@@ -5,54 +5,61 @@ import sys
 import codecs
 import xml.etree.ElementTree as ET
 
-tmpl_msg = '''
-message %(msgname)s
+############################################################################
+proto_msg = '''
+message %(module)s_%(msg_name)s%(msg_type)s
 {
 %(fields)s
 }\n
 '''
-
 # tmpl_field = '%(proto_type)s %(value_type)s %(field_name)s = %(tag)s'
 tmpl_field = '%s %s %s = %s;'
+############################################################################
 
 class GenProto(object):
     def __init__(self):
         self.import_files = []
         self.root = None
         self.proto_file = ""
+        self.module = ""
         pass
     
     def get_import_files(self):
         import_files = self.root.findall("Import")
+        self.module = self.root.attrib['name']
         for files in import_files:
             self.import_files.append(files.text)
         print(self.import_files)
         pass
 
-    def make_one_message(self, msg_name, msg):
+    def make_message(self, msg_name, msg, msg_type):
         field_str = ""
         for field in msg:
             field_str += tmpl_field % (field.attrib["proto_type"],field.attrib["value_type"],field.attrib["field_name"],field.attrib["tag"])
             field_str +='\n'
-        message = tmpl_msg % {"msgname":msg_name, "fields":field_str}    
+        if msg_type == 1:
+            message = proto_msg % {"module":self.module, "msg_name":msg_name, "fields":field_str, "msg_type":"Req"}    
+        elif msg_type == 2:
+            message = proto_msg % {"module":self.module, "msg_name":msg_name, "fields":field_str, "msg_type":"Reply"}    
+        elif msg_type == 3:
+            message = proto_msg % {"module":self.module, "msg_name":msg_name, "fields":field_str, "msg_type":"Notify"}    
         return message
 
     def get_proto_contents(self):
         message_contents = ""
-        for replymsg in self.root.findall("Message/ReplyMsg"):
+        for req_reply in self.root.findall("Message/ReqReplyMsg"):
+            msg_name = req_reply.attrib['name']
             # get req message
-            req = replymsg[0]
-            req_msgname = req.attrib['name']
-            message_contents += self.make_one_message(req_msgname, req)
+            req = req_reply[0]
+            message_contents += self.make_message(msg_name, req, 1)
             # get reply message
-            reply = replymsg[1]
-            reply_msgname = reply.attrib['name']
-            message_contents += self.make_one_message(reply_msgname, reply)           
+            reply = req_reply[1]
+            message_contents += self.make_message(msg_name, reply, 2)           
         
-        for notifymsg in self.root.findall("Message/NotifyMsg"):
-            for notify in notifymsg:
+        for notify_msg in self.root.findall("Message/NotifyMsg"):
+            for notify in notify_msg:
                 notify_name = notify.attrib["name"]
-                message_contents += self.make_one_message(notify_name, notify)         
+                message_contents += self.make_message(notify_name, notify, 3)         
         
         return message_contents
         pass
@@ -71,21 +78,18 @@ class GenProto(object):
         pass
 
     def write_proto(self):
+        self.get_import_files()
         proto_contents = 'syntax = "proto3";\n'
-        for other in self.import_files:
-            proto_contents += 'import "%s";\n' % other
+        for imp_file in self.import_files:
+            proto_contents += 'import "%s";\n' % imp_file
 
         proto_contents += self.get_proto_contents()
         with open(self.proto_file, 'w+') as f:
             f.write(proto_contents + '\n')
         pass
 
-
 if __name__ == "__main__":
     gen_proto = GenProto()
     gen_proto.load_xml("ModuleChat.xml")
-    gen_proto.get_import_files()
-    gen_proto.get_proto_contents()
     gen_proto.write_proto()
-    
     pass
