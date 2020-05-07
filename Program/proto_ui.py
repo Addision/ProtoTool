f53e2d5639b9ed10b5c6c4c6d973e2a8b97532a2
laijia <laijia2008@126.com>
2020-05-07 14:58:45 +0800

    delete files

--- Program/proto_ui.py
+++ Program/proto_ui.py
@@ -13,6 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.WindowModal)
         MainWindow.resize(828, 776)
         MainWindow.setAutoFillBackground(True)
         MainWindow.setStyleSheet("")
